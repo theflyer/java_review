18de36e10233ac3d3c57d583f7dcdeb7b9c95529
Asif Saifullah <asif@Asifs-MacBook-Air-2.local>
2019-01-01 22:39:13 -0500

    new2

---
+++ Emart _3/src/Assignment4/Certification.java
@@ -0,0 +1,12 @@
+package Assignment4;
+
+public interface Certification {
+
+	double REG_COURSE_FEE = 2000.0;
+	double CRASH_COURSE_FEE  = 5000.0;
+	
+	//double caculateFee();
+
+	double calculateFee();
+	
+}
---
+++ Emart _3/src/Assignment4/tester.java
@@ -0,0 +1,13 @@
+package Assignment4;
+
+public class tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RRTechnicalCertification r1 = new RegularCourseCertification("Rakesh","J2EE",85,5);
+		r1.display();
+		RRTechnicalCertification r2 = new CrashCourseCertification("Prity","Angular",71);
+		r2.display();
+	}
+
+}
